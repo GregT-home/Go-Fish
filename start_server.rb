32ce805396e70afbff1858e1b382872aa572673c
Greg Tarsa <gltarsa@gmail.com>
2014-01-01 18:48:43 -0500

    Now can take number of players from command line.

--- start_server.rb
+++ start_server.rb
@@ -1,9 +1,16 @@
 require_relative "./FishServer.rb"
 
-print "How many players for this game? "
-num = gets.chomp.strip.to_i
+# take # of players from command line, if present
+num = ARGV.length > 0 ? ARGV[0].to_i : 0
 
-server = FishServer(num)
+# get it from terminal, if not in command line
+until num >= 2 && num <= 10
+  print "How many players for this game (2-10)? "
+  num = gets.chomp.strip.to_i
+end
+
+puts "Creating a Fish Server for #{num} players..."
+server = FishServer.new(num)
 
 server.run
 
